15500235964a3711558a31b47d12373d01611cea
Venkat Narayan <vnarayan@lm-dkq9mw0tlg.lan>
2025-03-08 20:50:12 -0800

    Configured mongoDB Atlas

--- server.js
+++ server.js
@@ -6,15 +6,24 @@ const { json } = pkg;
 
 const app = express();
 const port = 5001;
+const mongoose = require("mongoose");
 
 // Middleware
 app.use(cors());
 app.use(json());
 
+// Use Heroku Config Var or fallback to local MongoDB
+const mongoURI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/myLocalDB";
+
 // MongoDB connection
-connect("mongodb://localhost:27017/rentalApp", { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
+//connect("mongodb://localhost:27017/rentalApp", { useNewUrlParser: true, useUnifiedTopology: true })
+//  .then(() => console.log("Connected to MongoDB"))
+//  .catch((err) => console.log(err));
+
+mongoose
+    .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("✅ MongoDB Connected Successfully"))
+    .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
 // Schema for rental details
 const rentalSchema = new Schema({
