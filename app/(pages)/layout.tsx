c71056ce448ff7caa2f5964bbf8da7eb644751d4
Hardik <venky79@gmail.com>
2025-05-05 18:45:12 +0530

    Added google tag

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -18,6 +18,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z8G81VZVGZ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Z8G81VZVGZ');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`antialiased`}
       >
