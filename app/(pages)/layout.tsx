a009a08f57015f63cbaef89195dfb6073c863c27
Denise <denisehsx01@gmail.com>
2025-06-17 23:35:10 +0530

    Added chat feature

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from "next";
 import Script from "next/script";
 import { Montserrat } from "next/font/google";
@@ -39,6 +40,26 @@ export default function RootLayout({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-Z8G81VZVGZ');
+          `}
+        </Script>
+        {/* Tawk.to Chat Widget */}
+        <Script id="tawk-init" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{};
+            Tawk_API.onLoad = function(){
+              Tawk_API.setAttributes({
+                widgetColor: '#2563eb'
+              });
+            };
+            var Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/684fbd8f57eabb190a266542/1itrnov2n';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
           `}
         </Script>
       </head>
