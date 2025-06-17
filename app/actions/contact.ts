2e5cf393b339b412ff07631b2e5548c52ae8a2ce
Denise <denisehsx01@gmail.com>
2025-06-17 23:41:22 +0530

    Update contact.ts

--- app/actions/contact.ts
+++ app/actions/contact.ts
@@ -6,7 +6,9 @@ const resendApiKey = process.env.RESEND_API_KEY;
 
 if (!resendApiKey) {
   throw new Error("RESEND_API_KEY is not defined in the environment variables.");
-}
+} 
+
+
 
 const resend = new Resend(resendApiKey);
 
